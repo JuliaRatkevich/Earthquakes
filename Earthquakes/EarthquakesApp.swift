8e4f37827ab1d50f6acdc7b0c5227c8c2948d124
Julia <juliakuntsevich@gmail.com>
2022-11-08 21:09:49 +0100

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Yuliya Ratkevcih on 08.11.22.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
